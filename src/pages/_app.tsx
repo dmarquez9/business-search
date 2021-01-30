1b13729175470391ef3b098d1a5affd29992b33e
Daniel Marquez <danielmrqz96@gmail.com>
2021-01-30 16:45:50 -0400

    Add Apollo Client & Redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { ApolloProvider } from '@apollo/client';
+import { useStore } from '../lib/redux';
+import { useApollo } from '../lib/apollo';
+
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const store = useStore(pageProps.initialReduxState)
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  return (
+    <Provider store={store}>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </Provider>
+  )
 }
 
 export default MyApp
