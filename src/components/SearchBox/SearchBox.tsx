7328d57a0827745e0c72a114299e25604464b472
Daniel Marquez <danielmrqz96@gmail.com>
2021-01-31 12:52:38 -0400

    Add logic to change term on SearchBox

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,10 +1,30 @@
+import React from 'react';
 import styles from './SearchBox.module.scss';
+import { useDispatch } from 'react-redux';
 
 function SearchBox() {
+  const dispatch = useDispatch()
+  const [form, setForm] = React.useState({
+    term: '',
+    location: ''
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: 'CHANGE_TERM', term: form.term })
+  }
+
+  const handleInputChange = (event) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      [event.target.name] : event.target.value
+    }))
+  }
+
   return (
-    <form className={styles.searchForm}>
-      <input type="text" placeholder="Search..." />
-      <input type="text" placeholder="Location" />
+    <form className={styles.searchForm} onSubmit={handleSubmit}>
+      <input type="text" name="term" value={form.term} placeholder="Search..." onChange={handleInputChange} required />
+      <input type="text" name="location" value={form.location} placeholder="Location" onChange={handleInputChange} required />
       <button type="submit">
         <svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 512 512">
           <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
